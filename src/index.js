75790ec5c2d1de007460228f8df85b7e34e89d2f
Sergey Zhmudikov <32043463+SergeyZhmudikov@users.noreply.github.com>
2018-02-26 16:19:22 +0300

    use cycles for solution.

--- src/index.js
+++ src/index.js
@@ -3,5 +3,25 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+    // your implementation
+    var trio = 0;
+
+    for (var i = 0; i < preferences.length; i++) {
+        var a = preferences[i];
+        for (var j = i + 1; j < preferences.length; j++) {
+            if (j + 1 === a) {
+                var b = preferences[j];
+                for (var k = i + 1; k < preferences.length; k++) {
+                    if (k + 1 === b) {
+                        var c = preferences[k];
+                        if (c === i + 1) {
+                            trio++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return trio;
+}
